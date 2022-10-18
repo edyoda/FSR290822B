b60bcc42fd36a79079ddf0592160a065ab873862
HiralKhakhariya <112894926+HiralKhakhariya@users.noreply.github.com>
2022-10-18 09:48:42 +0530

    Add files via upload

---
+++ Js Assignment 3.js
@@ -0,0 +1,61 @@
+var newReleases = [
+{
+"id": 70111470,
+"title": "Die Hard",
+"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+"rating": [4.0],
+"bookmark": []
+},
+{
+"id": 654356453,
+"title": "Bad Boys",
+"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+"rating": [5.0],
+"bookmark": [{ id: 432534, time: 65876586 }]
+},
+{
+"id": 65432445,
+"title": "The Chamber",
+"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+"rating": [4.0],
+"bookmark": []
+},
+{
+"id": 675465,
+"title": "Fracture",
+"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+"rating": [5.0],
+"bookmark": [{ id: 432534, time: 65876586 }]
+}
+]
+// 1- read only id and title and push into blank array 
+// 2-  print bookmark which has a data 
+// 3 -  print rating if it is greater than 4
+
+
+// Q.s 2 
+ var = [
+{
+id : 1,
+name :"abc"
+},
+{
+id : -2,
+name :"pqr"
+},
+{
+id : -1,
+name :"xyz"
+},
+{
+id : 2,
+name :"mno"
+},
+
+
+]
+// print positive id only
