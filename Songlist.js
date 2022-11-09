374f0cefd20ab4f5d8791e218c5a2a61c517ce39
HiralKhakhariya <112894926+HiralKhakhariya@users.noreply.github.com>
2022-11-09 20:24:49 +0530

    Create Songlist.js

---
+++ Songlist.js
@@ -0,0 +1,39 @@
+
+
+var songs = [
+    {
+       img: "https://www.rafaelalucas.com/dailyui/9/covers/jain.jpg",
+       artistName: "Jain",
+       songName: "Makeba",
+       song: "https://www.rafaelalucas.com/dailyui/9/songs/song1.mp3",
+       color: "#f5c63d"
+    },
+    {
+       img: "https://www.rafaelalucas.com/dailyui/9/covers/ohland.jpg",
+       artistName: "Oh Land",
+       songName: "Postpone the Bad",
+       song: "https://www.rafaelalucas.com/dailyui/9/songs/song2.mp3",
+       color: "#afc5c3"
+    },
+    {
+       img: "https://www.rafaelalucas.com/dailyui/9/covers/angele.jpg",
+       artistName: "Angèle",
+       songName: "La Loi de Murphy",
+       song: "https://www.rafaelalucas.com/dailyui/9/songs/song3.mp3",
+       color: "#74c2dd"
+    },
+    {
+       img: "https://www.rafaelalucas.com/dailyui/9/covers/broods.jpg",
+       artistName: "Broods",
+       songName: "Bridges",
+       song: "https://www.rafaelalucas.com/dailyui/9/songs/song1.mp3",
+       color: "#a3b8b0"
+    },
+   
+ ];
+
+
+
+
+
+
